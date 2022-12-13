63df0bf030b0a1d80fd61bfb6c4d3c92e107faca
ryan-stoltz <119019260+ryan-stoltz@users.noreply.github.com>
2022-12-13 11:45:37 -0500

    Update website.js

--- static/js/website.js
+++ static/js/website.js
@@ -1,8 +1,16 @@
+
+//Bar animation
+
 $(".bar-inner").each(function(){
     $(this).animate({
       width: $(this).parent().attr("data-width")
     },2000)
   });
+
+
+
+
+//Countries
 
 var c1 = $("#Country1").attr('name');
 var c2 = $("#Country2").attr('name');
@@ -12,9 +20,6 @@ var c5 = $("#Country5").attr('name');
 var c6 = $("#Country6").attr('name');
 var c7 = $("#Country7").attr('name');
 var c8 = $("#Country8").attr('name');
-
-
-
 
 
 $(".name1").hover(function(){
@@ -74,6 +79,65 @@ $(".name8").hover(function(){
 },function(){
   $(".countrytext").text($(".countrytext").text().replace("Country Name: " + c8, "Country Name:"));
 });
+
+
+//Contact Form
+
+
+const form = document.getElementById("form");
+
+const res = document.getElementById("sent");
+
+
+form.addEventListener("submit", function (e) {
+
+  const formData = new FormData(form);
+  e.preventDefault();
+  var object ={};
+  formData.forEach((value,key) => {
+    object[key] = value;
+  });
+
+  var json = JSON.stringify(object);
+  res.innerHTML = "One Second...";
+
+  fetch("https://api.web3forms.com/submit" , {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: json
+
+  })
+  
+  .then(async (response) => {
+    let json = await response.json();
+    if (response.status == 200) {
+      res.innerHTML = json.message;
+        res.classList.remove("text-gray-500");
+        res.classList.add("text-green-500");
+      } else {
+        console.log(response);
+        res.innerHTML = json.message;
+        res.classList.remove("text-gray-500");
+        res.classList.add("text-red-500");
+      }
+  })
+
+  .catch((error) => {
+    console.log(error);
+    res.innerHTML = "Something went wrong!";
+  })
+
+  .then(function () {
+    form.reset();
+    setTimeout(() => {
+      res.style.display = "none";
+    }, 5000);
+  });
+});
+
 
 
 
